3ec7b0aa108abbb2156b105547437e092ef71251
Usama Rahman <95021960+usama-rahman@users.noreply.github.com>
2023-12-16 06:40:07 +0600

    @

--- src/AppV.1.jsx
+++ src/AppV.1.jsx
@@ -322,7 +322,7 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
       async function getMovieDetails() {
         setIsLoading(true);
         const res = await fetch(
-          `http://www.omdbapi.com/?apikey=${KEY}&s=${selectedId}`
+          `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
         );
         const data = await res.json();
         setMovie(data);
